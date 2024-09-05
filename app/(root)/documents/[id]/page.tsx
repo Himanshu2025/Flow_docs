6d0b574b01c0d8bb6a6f74001443e90dbc6f432c
Himanshu2025 <hkulkarni927@gmail.com>
2024-09-05 18:01:17 +1000

    Added heder component and types

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 const Document = () => {
   return (
-    <div> Document</div>
+    <div>
+      <Header>
+        <div>
+          <p className='title'> Test Document</p>
+        </div>
+      </Header>
+      <Editor/>
+    </div>
+    
   )
 }
 
